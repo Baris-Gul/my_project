d8eccffc018ec538ed9519f87891b18044ef4b98
Baris-Gul <93883851+Baris-Gul@users.noreply.github.com>
2021-11-29 20:22:10 +0100

    Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=0
+y=1
+liste=[]
+a=0
+while a<50:
+    z=x+y
+    x=y
+    y=z
+    liste.append(z)
+    if liste[-1]==55:
+        print(liste)
+        break
+    a+=1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
