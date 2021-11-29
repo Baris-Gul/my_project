dcfa4317b8eb3289a2742e299979349d3bcb74ce
Baris-Gul <93883851+Baris-Gul@users.noreply.github.com>
2021-11-29 21:09:02 +0100

    Assignment - 1 (Doubler)

---
+++ Assignment - 1 (Doubler).py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+import random
+def a(x,y):
+    if x==y:
+        print(2*(x+y))
+    else:
+        print(x+y)
+while True:
+    sayi1=random.randint(1,100)
+    print(sayi1)
+    sayi2=random.randint(1,100)
+    print(sayi2)
+    a(sayi1,sayi2)
+    if sayi1==sayi2:
+        break
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
